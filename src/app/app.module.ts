dbde33c1a44c54019ee6c23769d8e2f5d059aa2c
Gabriel Batistuta <batistutag190@gmail.com>
2022-09-06 14:57:15 -0300

    paginas footer, header e pages criadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    NewMomentComponent
   ],
   imports: [
     BrowserModule,
